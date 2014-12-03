408e056bfa7c854ca91209a2d82742d8f03c894e
Jeff White <jwhite530@gmail.com>
2014-12-03 09:58:20 -0500

    Removing license information from files and added file 'LICENSE'

--- bin/beomon_master_agent.py
+++ bin/beomon_master_agent.py
@@ -1,15 +1,5 @@
 #!/opt/sam/python/2.7.5/gcc447/bin/python
 # Description: Beomon master agent
-# Written by: Jeff White of the University of Pittsburgh (jaw171@pitt.edu)
-
-
-
-# License:
-# This software is released under version three of the GNU General Public License (GPL) of the
-# Free Software Foundation (FSF), the text of which is available at http://www.fsf.org/licensing/licenses/gpl-3.0.html.
-# Use or modification of this software implies your acceptance of this license and its terms.
-# This is a free software, you are free to change and redistribute it with the terms of the GNU GPL.
-# There is NO WARRANTY, not even for FITNESS FOR A PARTICULAR USE to the extent permitted by law.
 
 
 
